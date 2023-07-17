078debb7f0ca3398607ea0e4a39186420925e53a
Zhuo Song <sz@zack01.gqy3zvrduhruff4ezhlwengybh.ax.internal.cloudapp.net>
2023-07-17 10:03:04 +0000

    0717

--- test/t-azure-rule-clean.py
+++ test/t-azure-rule-clean.py
@@ -35,20 +35,32 @@ _info = readF(_dir, '_ans44')
 
 clean_template = \
 """
-Giving the following list:
+Giving the following rule list:
 --------------------
 {info}
 --------------------
-Please remove ALL duplicate entries in the list:
+Please check and remove ALL duplicate rules in the list, make sure the rules in the list are unique:
 """
 clean_prompt = PromptTemplate.from_template(clean_template)
+
+_ans5_1, _step5_1 = "", ""
 with get_openai_callback() as cb:
     llm = ChatOpenAI(model_name=os.getenv('OPENAI_MODEL'), temperature=0)
     chain = LLMChain(llm=llm, prompt=clean_prompt)
     _re = chain.run(info=_info)
     _token_cost = f"Tokens: {cb.total_tokens} = (Prompt {cb.prompt_tokens} + Completion {cb.completion_tokens}) Cost: ${format(cb.total_cost, '.5f')}"
-    _ans5 = _re.strip()
-    _step5 = f"{_token_cost}\n\n" + "="*20+" prompt "+"="*20+"\n" + clean_prompt.format(info=_info)
-writeF(_dir, '_ans55', _ans5)
-writeF(_dir, '_step55', _step5)
+    _ans5_1 = _re.strip()
+    _step5_1 = f"{_token_cost}\n\n" + "="*20+" prompt "+"="*20+"\n" + clean_prompt.format(info=_info)
+
+_ans5_2, _step5_2 = "", ""
+with get_openai_callback() as cb:
+    llm = ChatOpenAI(model_name=os.getenv('OPENAI_MODEL'), temperature=0)
+    chain = LLMChain(llm=llm, prompt=clean_prompt)
+    _re = chain.run(info=_ans5_1)
+    _token_cost = f"Tokens: {cb.total_tokens} = (Prompt {cb.prompt_tokens} + Completion {cb.completion_tokens}) Cost: ${format(cb.total_cost, '.5f')}"
+    _ans5_2 = _re.strip()
+    _step5_2 = f"{_token_cost}\n\n" + "="*20+" prompt "+"="*20+"\n" + clean_prompt.format(info=_ans5_1)
+
+writeF(_dir, '_ans55', _ans5_2)
+writeF(_dir, '_step55', _step5_1+"\n\n"+_step5_2)
 
